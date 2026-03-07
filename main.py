81d8b829eea5e7ec8acf75b65e8bf841d2b087d9
mrgreenery <wilbergroen@gmail.com>
2026-03-07 19:51:45 +0100

    cleaned up stucture

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import monitor
+from monitor import Monitor
+
+def main():
+    system_monitor = Monitor()
+    system_monitor.run()
 
 if __name__ == "__main__":
-    m = monitor.Monitor()
-    m.run()
+    main()
