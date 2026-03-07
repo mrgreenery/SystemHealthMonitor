2866d26447ab250eb0126fd03fcc0805d2ccb955
mrgreenery <wilbergroen@gmail.com>
2026-03-07 19:44:42 +0100

    updated check_metric, to make metrics in config expendable

--- evaluator.py
+++ evaluator.py
@@ -1,6 +1,14 @@
 from config import THRESHOLDS
 
+
 class Evaluator:
-        def check_metric(self, name, value):
-            status = "ALERT" if value >= THRESHOLDS[name] else "OK"
-            return status
+    def check_metric(self, name, value):
+        threshold = THRESHOLDS.get(name)
+
+        if threshold is None:
+            return "-"
+
+        if value >= threshold:
+            return "ALERT"
+
+        return "OK"
